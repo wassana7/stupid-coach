ec08ea97f57b1fa4c6858e6e4176eb3e2e04cd7b
Wassana Sengsavang <wassana.seng@gmail.com>
2021-11-08 12:57:52 +0100

    Stupid coaching maj

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == 'i am going to work right now!'
+    return @answer == 'Great!'
+    elsif @question.end_with?('?')
+    return @answer == 'Silly question, get dressed and go to work!'
+    else
+    return @answer == "I don't care, get dressed and go to work!"
+    end
+  end
+end
